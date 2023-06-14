f37f130c6a5a485af4500606d849623d60cc3dbb
Hirofumi Seiyama <hseiyama@gmail.com>
2023-06-15 00:51:47 +0900

    デバッグ機能を整理。RFSH端子のモニタ追加。

--- arduino/z80mega1_clock/z80mega1_clock.ino
+++ arduino/z80mega1_clock/z80mega1_clock.ino
@@ -18,14 +18,19 @@
 //  Arduino Pin 2  <--->  Clock Button
 //  Arduino Pin 3  <--->  Reset Button
 // Memory map:
-//  0000-00FF  Ram   255
+//  0000-00FF  Ram   256
 // --------------------------------------------------------------------------------
 
 // Ram memory shared with Z80 mapped 0000H-00FFH
 uint8_t RAM[0x0100];  // Emulated RAM memory
 uint8_t IO[0x0100];   // Emulated IO memory
 
-// Pin difines
+// Common defines
+#define OFF           (0)
+#define ON            (1)
+#define ON2           (2)     // 特殊フラグとして利用
+
+// Pin defines
 #define ADDR          PINA
 #define DATA_R        PINL
 #define DATA_W        PORTL
@@ -35,6 +40,7 @@ uint8_t IO[0x0100];   // Emulated IO memory
 #define MREQ_PIN      PK1
 #define IORQ_PIN      PK2
 #define M1_PIN        PK5
+#define RFSH_PIN      PK6
 #define WAIT_PIN      PG0
 #define RESET_PIN     PG1
 #define INT_PIN       PG2
@@ -47,11 +53,13 @@ uint8_t IO[0x0100];   // Emulated IO memory
 #define MREQ          ((PINK & (1 << MREQ_PIN)) == 0)
 #define IORQ          ((PINK & (1 << IORQ_PIN)) == 0)
 #define M1            ((PINK & (1 << M1_PIN)) == 0)
+#define RFSH          ((PINK & (1 << RFSH_PIN)) == 0)
 #define U_CLK         ((PINE & (1 << U_CLK_PIN)) == 0)
 #define U_RST         ((PINE & (1 << U_RST_PIN)) == 0)
 
 // Drive pins
 #define SET_CLK(a)    (a == 0 ? PORTB |= (1 << CLK_PIN) : PORTB &= ~(1 << CLK_PIN))
+#define REV_CLK()     (PINB = (1 << CLK_PIN))
 #define SET_WAIT(a)   (a == 0 ? PORTG |= (1 << WAIT_PIN) : PORTG &= ~(1 << WAIT_PIN))
 #define SET_RESET(a)  (a == 0 ? PORTG |= (1 << RESET_PIN) : PORTG &= ~(1 << RESET_PIN))
 #define SET_INT(a)    (a == 0 ? PORTG |= (1 << INT_PIN) : PORTG &= ~(1 << INT_PIN))
@@ -63,21 +71,27 @@ uint8_t IO[0x0100];   // Emulated IO memory
 #define PORT_MASK_G   ((1 << WAIT_PIN) + (1 << RESET_PIN) + (1 << INT_PIN))
 
 // Program hex code
-uint8_t Program[] = {
+static const uint8_t Program[] = {
+/*
   0x3E, 0x03,         // LD A,3
   0xC6, 0x04,         // ADD A,4
   0x32, 0x08, 0x00,   // LD (BUFF),A
   0x76,               // HALT
   // BUFF:
   0xFF                // DEFB 0FFh
-/*
+*/
   // START:
   0xDB, 0x03,         // IN A,(03h)
   0xC6, 0x04,         // ADD A,4
   0xD3, 0x03,         // OUT (03h),A
   0x18, 0xF8          // JR START
-*/
 };
+
+// Flag for debug
+static uint8_t debug_clock;
+
+static void echo_dump(uint8_t*);
+static void debug_req(void);
 
 void setup() {
   // put your setup code here, to run once:
@@ -89,14 +103,59 @@ void setup() {
   PORTE = PORT_MASK_E;    // プルアップ設定 (U_CLK, U_RST)
   DDRG = PORT_MASK_G;     // 出力方向 (WAIT, RESET, INT)
   PORTG = PORT_MASK_G;    // WAIT=ON, RESET=ON, INT=ON
-  DDRK = 0x00;            // 入力方向 (MREQ, IORQ, M1)
+  DDRK = 0x00;            // 入力方向 (MREQ, IORQ, M1, RFSH)
   DDRL = 0x00;            // 入力方向 (DATA)
-  // RAM初期化
-  memset(RAM, 0, sizeof(RAM));
-  memcpy(RAM, Program, sizeof(Program));
-  // IO初期化
-  memset(IO, 0, sizeof(IO));
+
+  memset(RAM, 0, sizeof(RAM));            // RAM初期化
+  memcpy(RAM, Program, sizeof(Program));  // Programをロード
+  memset(IO, 0, sizeof(IO));              // IO初期化
+
+  debug_clock = ON;       // デバッグ用のクロック設定
+
   Serial.begin(9600);	    // 9600bpsでポートを開く
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (debug_clock == ON) {
+    SET_CLK(U_CLK);
+  }
+  else {
+    REV_CLK();            // CLOCK反転
+  }
+  SET_RESET(U_RST);
+
+  if (MREQ && RD) {
+    DDRL = 0xFF;          // 出力方向 (DATA)
+    DATA_W = RAM[ADDR];   // DATA <- RAM
+    debug_log("MREQ_RD", ON);
+  }
+  else if (MREQ && WR) {
+    DDRL = 0x00;          // 入力方向 (DATA)
+    RAM[ADDR] = DATA_R;   // RAM <- DATA
+    debug_log("MREQ_WR", ON);
+  }
+  else if (IORQ && RD) {
+    DDRL = 0xFF;          // 出力方向 (DATA)
+    DATA_W = IO[ADDR];    // DATA <- IO
+    debug_log("IORQ_RD", ON);
+  }
+  else if (IORQ && WR) {
+    DDRL = 0x00;          // 入力方向 (DATA)
+    IO[ADDR] = DATA_R;    // IO <-  DATA
+    debug_log("IORQ_WR", ON);
+  }
+  else if (RFSH && MREQ) {
+    DDRL = 0x00;          // 入力方向 (DATA)
+    debug_log("RFSH_MREQ", ON2);
+  }
+  else {
+    DDRL = 0x00;          // 入力方向 (DATA)
+    debug_log("", OFF);
+  }
+
+  debug_req();
+  delay(5);
 }
 
 static void echo_hex(uint8_t hex_data) {
@@ -107,81 +166,56 @@ static void echo_hex(uint8_t hex_data) {
   Serial.write(hex_table[hex_data & 0x0F]);
 }
 
-static void echo_dump(uint8_t *range) {
-  Serial.println(">>> 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F");
+static void echo_dump(uint8_t* addr_hi, uint8_t* range) {
+  Serial.println("     | 00 01 02 03 04 05 06 07  08 09 0A 0B 0C 0D 0E 0F");
+  Serial.println("-----+-------------------------------------------------");
   for (uint8_t index_i=0; index_i<16; index_i++) {
+    Serial.print((const char[])addr_hi);
     echo_hex(index_i * 16);
-    Serial.print(">");
+    Serial.print(" |");
     for (uint8_t index_j=0; index_j<16; index_j++) {
       Serial.print(" ");
+      if (index_j == 8) {
+        Serial.print(" ");
+      }
       echo_hex(range[index_i * 16 + index_j]);
     }
     Serial.println("");
   }
 }
 
-static void dump_req(void) {
+static void debug_req(void) {
   int8_t rcv_data = Serial.read();
-  if (rcv_data == 'm') {
-    Serial.println("RAM[] =");
-    echo_dump(&RAM[0]);
+  if (rcv_data == 'm') {      // RAM領域をダンプ
+    Serial.println("RAM[ 0x0000-0x00FF ] =");
+    echo_dump("00", &RAM[0]);
   }
-  else if (rcv_data == 'i') {
-    Serial.println("IO[] =");
-    echo_dump(&IO[0]);
+  else if (rcv_data == 'i') { // IO領域をダンプ
+    Serial.println("IO[ 0x00-0xFF ] =");
+    echo_dump("  ", &IO[0]);
+  }
+  else if (rcv_data == 'c') { // クロック設定を変更
+    debug_clock = !debug_clock;
   }
 }
 
-
-static void echo_event(uint8_t *msg, uint8_t flag) {
-  static uint8_t event_flag = 0;
-  if (flag == 0) {
-    event_flag = 0;
+static void debug_log(uint8_t* msg, uint8_t flag) {
+  static uint8_t event_flag = OFF;
+  if (flag == OFF) {
+    event_flag = OFF;
   }
-  else if (event_flag == 0) {
-    event_flag = 1;
+  else if (event_flag == OFF) {
+    event_flag = ON;
     Serial.print((const char[])msg);
     Serial.print(": ADDR=0x");
     echo_hex(ADDR);
-    Serial.print(", DATA=0x");
-    echo_hex(DATA_R);
+    if (flag == ON) {
+      Serial.print(", DATA=0x");
+      echo_hex(DATA_R);
+    }
     if (M1) {
       Serial.print(" <M1>");
     }
     Serial.println("");
   }
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  SET_CLK(U_CLK);
-  SET_RESET(U_RST);
-
-  if (MREQ && RD) {
-    DDRL = 0xFF;          // 出力方向 (DATA)
-    DATA_W = RAM[ADDR];   // DATA <- RAM
-    echo_event("MREQ_RD", 1);
-  }
-  else if (MREQ && WR) {
-    DDRL = 0x00;          // 入力方向 (DATA)
-    RAM[ADDR] = DATA_R;   // RAM <- DATA
-    echo_event("MREQ_WR", 1);
-  }
-  else if (IORQ && RD) {
-    DDRL = 0xFF;          // 出力方向 (DATA)
-    DATA_W = IO[ADDR];    // DATA <- IO
-    echo_event("IORQ_RD", 1);
-  }
-  else if (IORQ && WR) {
-    DDRL = 0x00;          // 入力方向 (DATA)
-    IO[ADDR] = DATA_R;    // IO <-  DATA
-    echo_event("IORQ_WR", 1);
-  }
-  else {
-    DDRL = 0x00;          // 入力方向 (DATA)
-    echo_event("", 0);
-  }
-
-  dump_req();
-  delay(5);
 }
