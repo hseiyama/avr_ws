853f98196614a7437cebf8742fa6490c70105c2b
Hirofumi Seiyama <hseiyama@gmail.com>
2023-07-16 22:51:21 +0900

    [ATmega328P]サンプルにUSART機能を追加

--- mplabx/mega328p.X/avr-main.c
+++ mplabx/mega328p.X/avr-main.c
@@ -13,6 +13,7 @@
 #define PORTD_MASK_OUT   (0b00001100)   // portD : 2,3bit目のみ出力
 
 volatile uint8_t portD_timer;           // タイマ割り込みからの指示用
+volatile uint8_t usart_data;            // USART送受信データ
 
 int main(void) {
     /* Replace with your application code */
@@ -20,6 +21,7 @@ int main(void) {
     
     /* 初期化 */
     portD_timer = 0x00;
+    usart_data = 0x00;
     /* PORT初期化 */
     DDRD = PORTD_MASK_OUT;      // 出力方向 (2,3bit)
     PORTD |= PORTD_MASK_IN;     // プルアップ抵抗を有効 (4,5bit)
@@ -29,6 +31,14 @@ int main(void) {
                                 // clkI/O/64 (64分周)
     OCR1A = 15625;              // 1000msごとに割り込み (OCR1AH,OCR1AL)
     TIMSK1 = (1 << OCIE1A);     // ﾀｲﾏ/ｶｳﾝﾀ1比較A割り込み許可
+    /* USART初期化 */
+    UBRR0 = 12;                 // ボーレート9600bps (1MHz,U2X=1,誤差=0.2%)
+    UCSR0A = (1 << U2X0);       // 倍速許可
+    UCSR0B = (1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0);
+                                // 受信完了割り込み許可
+                                // 受信許可, 送信許可
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+                                // 非同期動作,ﾊﾟﾘﾃｨ禁止,停止1ﾋﾞｯﾄ,ﾃﾞｰﾀ8ﾋﾞｯﾄ
     
     sei();                      // 割り込み許可
     while (1) {
@@ -40,4 +50,10 @@ int main(void) {
 /* タイマ割り込み */
 ISR(TIMER1_COMPA_vect, ISR_BLOCK) {
     portD_timer = ~portD_timer;
+}
+
+/* USART受信完了割り込み */
+ISR(USART_RX_vect, ISR_BLOCK) {
+  usart_data = UDR0;    // 受信データを取得
+  UDR0 = usart_data;    // 送信データを設定
 }
